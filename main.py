96415fbe0d1ed8a1d1141e7030c4252bc4d04471
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-03-06 20:54:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,11 @@ def time():
   string = strftime("%H:%M:%S %P")
   label.config(text=string)
   label.after(1000, time)
-  
+
+wake_up = input("wake up at: ")
 label = Label(root, font=("ds-digital", 80), background = "cyan", foreground = "red")
 label.pack(anchor="center")
 time()
 mainloop()
+if time == "wake_up":
+  print("Wake up")
